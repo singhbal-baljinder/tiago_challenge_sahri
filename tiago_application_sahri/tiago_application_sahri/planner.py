4ddb487110911f0ba4ef97a8a852ddfec821c9d8
dhimiter <dhimpik@proton.me>
2025-06-20 09:26:09 +0000

    clean

--- tiago_application_sahri/tiago_application_sahri/planner.py
+++ tiago_application_sahri/tiago_application_sahri/planner.py
@@ -34,47 +34,7 @@ class PosePublisher(Node):
         ]
 
         # Initialize pose list and current index
-        self.pose_list = self.create_pose_list()
         self.current_pose_index = 0
-
-    def create_pose_list(self):
-        """Create and return a list of target poses"""
-        poses = []
-
-        # Pose 1
-        pose1 = Pose()
-        pose1.position.x = 0.5
-        pose1.position.y = 0.0
-        pose1.position.z = 0.5
-        pose1.orientation.x = 0.0
-        pose1.orientation.y = 0.0
-        pose1.orientation.z = 0.0
-        pose1.orientation.w = 1.0
-        poses.append(pose1)
-
-        # Pose 2
-        pose2 = Pose()
-        pose2.position.x = 0.40
-        pose2.position.y = 0.1
-        pose2.position.z = 0.5
-        pose2.orientation.x = 0.0
-        pose2.orientation.y = 0.0
-        pose2.orientation.z = 0.0
-        pose2.orientation.w = 1.0
-        poses.append(pose2)
-
-        # Pose 3
-        pose3 = Pose()
-        pose3.position.x = 0.45
-        pose3.position.y = 0.1
-        pose3.position.z = 0.45
-        pose3.orientation.x = 0.0
-        pose3.orientation.y = 0.0
-        pose3.orientation.z = 0.0
-        pose3.orientation.w = 1.0
-        poses.append(pose3)
-
-        return poses
 
     def timer_callback(self):
         # Get the current pose from the list
@@ -115,7 +75,6 @@ def main(args=None):
     timer = node.create_timer(timer_period, node.timer_callback)
 
     rclpy.spin(node)
-    # node.basic_layer_logic()
     node.destroy_node()
     rclpy.shutdown()
 
